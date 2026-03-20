b209a1da5c22e54965daae01831b569e838de80f
Gabriel Nóbrega <gabrielnsampa@gmail.com>
2026-03-20 18:40:41 -0300

    Add files via upload

---
+++ exercicioOne/ExercOne.java
@@ -0,0 +1,15 @@
+public class ExercOne {
+    public static void main(String[] args) {
+        //declaração e atribuição de valores 
+            String nome = "Gabriel da Silva Nóbrega"; 
+            int idade = 21; 
+            double altura = 1.77; 
+            
+        //exibição formatada no console 
+            System.out.println ("ExercOne"); 
+            System.out.println ("nome: " + nome); 
+            System.out.println ("idade:" + idade + "anos");
+            System.out.println ("altura: " + altura + "m"); 
+
+    }
+}
